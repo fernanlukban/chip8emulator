737f627e1ef4c34f2ddfb45c9366bba44c3faab5
Fernan Lukban <fernanlukban@gmail.com>
2019-08-24 12:38:45 -0700

    added basic members for Chip8 class

--- chip8.h
+++ chip8.h
@@ -1,2 +1,10 @@
-class Chip8Emulator {
+#define NUM_OF_REGISTERS 16
+#define MEMORY_SIZE_KB 4096
+
+class Chip8 {
+	uint16_t opcode;
+	uint8_t registers[NUM_OF_REGISTERS];
+	uint8_t memory[MEMORY_SIZE_KB];
+	uint16_t index;
+	uint16_t pc;
 };
